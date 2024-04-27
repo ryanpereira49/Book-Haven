6dd9478d2b919bf34fb0e59e39d079aa647dd814
paradox4994 <ryanpereira499@gmail.com>
2024-04-26 23:06:57 -0400

    Changed mongo connection to cloud from local and modified server to start only after database connects.

--- server/index.js
+++ server/index.js
@@ -1,20 +1,24 @@
-const express = require('express');
-const dotenv = require('dotenv').config()
-const {mongoose} = require('mongoose')
-const cookieParser = require('cookie-parser')
+const express = require("express");
+const dotenv = require("dotenv").config();
+const { mongoose } = require("mongoose");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
-// Database Connection
-mongoose.connect(process.env.MONGO_URL_LOCAL).then(() => console.log('Database Connected')).catch((err)=> console.log('Database Not Connected',err))
-
 // Middleware
-app.use(express.json())
-app.use(cookieParser())
-app.use(express.urlencoded({extended: false}))
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
 
-
-app.use('/', require('./routes/authRoutes'))
+app.use("/", require("./routes/authRoutes"));
 
 const PORT = process.env.port || 8000;
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+
+// Database Connection and server startup
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("[Database] : Connected"))
+  .catch((err) => console.log("[Database]: Not Connected: ", err))
+  .then(() => {
+    app.listen(PORT, () => console.log(`[Server]: Running on port ${PORT}`));
+  });
