835ed668726ee8a4a3291aa9a691dfcc5b8e122a
paradox4994 <ryanpereira499@gmail.com>
2024-06-30 14:00:25 -0400

    changed "class" to "classname"

--- client/src/components/BookElement.jsx
+++ client/src/components/BookElement.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
-export default function BookElement({image, name, author, price}) {
+export default function BookElement({image, name, author, price, isbn}) {
   return (
-    <div class="border border-black rounded-lg p-4">
-        <img class="w-full h-72 object-cover rounded-lg" src={image} alt="Book Image"/>
-        <div class="pt-3">
-            <h2 class="text-lg font-semibold">{name}</h2>
-            <p class="text-gray-600">{author}</p>
-            <p class="text-gray-800 font-semibold">${price}</p>
-            <button class="mt-4 w-full bg-black text-white py-2 px-4 rounded-lg hover:shadow-md">Add to Cart</button>
+    <div className="border border-black rounded-lg p-4">
+        <img className="w-full h-72 object-cover rounded-lg" src={'http://'+image} alt="Book Image"/>
+        <div className="pt-3">
+            <h2 className="text-lg font-semibold overflow-hidden text-nowrap">{name}</h2>
+            <p className="text-gray-600">{author}</p>
+            <p className="text-gray-800 font-semibold">${price}</p>
+            <button className="mt-4 w-full bg-black text-white py-2 px-4 rounded-lg hover:shadow-md">Add to Cart</button>
         </div>
     </div>
   );
