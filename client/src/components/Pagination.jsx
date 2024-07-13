84d4483d07df103d9c2388f3c88e02f7a9cf873b
paradox4994 <ryanpereira499@gmail.com>
2024-07-12 20:55:54 -0400

    initial commit

---
+++ client/src/components/Pagination.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export default function Pagination({totalPosts,postPerPage, setCurrentPage, currentPage}) {
+
+    let pages = []
+
+    for(let i=1; i <= Math.ceil(totalPosts/postPerPage); i++ ){
+        pages.push(i)
+    }
+
+  return (
+    <div>
+      {
+        pages.map((page,index)=>{
+            return (
+              <button
+                className={
+                  page == currentPage
+                    ? "border-2 border-black rounded-full px-3 py-1 mx-3 bg-black text-white"
+                    : "border-2 border-black rounded-full px-3 py-1 mx-3"
+                }
+                key={index}
+                onClick={() => setCurrentPage(page)}>
+                {page}
+              </button>
+            );
+        })
+      }
+    </div>
+  )
+}
