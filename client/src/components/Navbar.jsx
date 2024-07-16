2316f623fdfd6a71fe47c4d6dbb0e9c89b3a1b1f
paradox4994 <ryanpereira499@gmail.com>
2024-07-15 23:26:25 -0400

    Add Logout and cart

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import axios from 'axios'
 import { useNavigate, Link } from 'react-router-dom'
+import { useContext } from "react";
+import { UserContext } from "../context/userContext";
 
 export default function Navbar() {
 
   const navigate = useNavigate()
+
+  const { user, setUser } = useContext(UserContext);
+
+  async function handleLogout() {
+    try {
+      await axios.get('/logout');
+      setUser(null);
+      //navigate('/login');
+    } catch (error) {
+      console.error('Logout failed:', error);
+    }
+  }
+
 
   return (
    <div className="flex flex-row mx-4 px-4 pt-4">
@@ -16,8 +32,20 @@ export default function Navbar() {
       <button className="mx-4 py-1 text-lg" onClick={() => {navigate('/')}}>Search</button>
       <button className="mx-4 py-1 text-lg" onClick={() => {navigate('/')}}>Home</button>
       <button className="mx-4 py-1 text-lg" onClick={() => {navigate('/')}}>Categories</button>
-      <button className="mx-4 py-1 px-3  text-lg border-2 border-black rounded hover:bg-black hover:text-white " onClick={() => {navigate('/login')}}>Login</button>
-      <button className="mx-4 py-1 px-4  text-lg bg-black text-white rounded hover:shadow-md" onClick={() => {navigate('/register')}}>Sign Up</button>
+      {
+        user ? (
+        <>
+        <button className="mx-4 py-1 text-lg" onClick={() => {navigate('/')}}>Hi {user.username}!</button>
+        <button className="mx-4 py-1 px-4  text-lg bg-black text-white rounded hover:shadow-md" onClick={() => {navigate('/login')}}>Cart</button>
+        <button className="mx-4 py-1 px-3  text-lg border-2 border-black rounded hover:bg-black hover:text-white " onClick={handleLogout}>Log Out</button>
+        </>) : (
+          <>
+          <button className="mx-4 py-1 px-3  text-lg border-2 border-black rounded hover:bg-black hover:text-white " onClick={() => {navigate('/login')}}>Login</button>
+          <button className="mx-4 py-1 px-4  text-lg bg-black text-white rounded hover:shadow-md" onClick={() => {navigate('/register')}}>Sign Up</button>
+          </>
+        )
+      }
+      
     </div>
    </div>
   )
