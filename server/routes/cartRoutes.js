b209ae1bad4fe596665987e462dd914026961c76
paradox4994 <ryanpereira499@gmail.com>
2024-07-15 23:29:07 -0400

    Initital Commit

---
+++ server/routes/cartRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+const cors = require('cors')
+const {fetch, add, remove, increment, decrement} = require('../controllers/cartControllers')
+
+//Middleware
+router.use(
+    cors({
+        credentials: true,
+        origin: "http://localhost:5173"
+    })
+)
+
+router.post('/fetch', fetch)
+router.post('/add',add)
+router.post('/remove',remove)
+router.post('/increment',increment)
+router.post('/decrement',decrement)
+
+module.exports = router
