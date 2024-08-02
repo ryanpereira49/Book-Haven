a8ea37faee60f3f9f709b9cc63121642304b0705
paradox4994 <ryanpereira499@gmail.com>
2024-08-02 11:55:58 -0400

    Add user context

--- client/src/components/BookElement.jsx
+++ client/src/components/BookElement.jsx
@@ -1,11 +1,40 @@
 import React from "react";
 import { useNavigate, Link } from "react-router-dom";
+import { UserContext } from "../context/userContext";
+import { useContext } from "react";
+import toast from "react-hot-toast";
+import axios from "axios";
 
 
-export default function BookElement({image, name, author, price, isbn}) {
+export default function BookElement({image, name, author, price, isbn, inCart, onAddToCart}) {
 
   const navigate = useNavigate();
+  const { user, loading: userLoading } = useContext(UserContext);
 
+  async function AddToCart(product){
+    try{
+      const response = await axios.post("/cart/add", { username: user.username, product: product });
+      if(response.data.success){
+        toast.success(response.data.success)
+        onAddToCart()
+      }
+    }catch(error){
+      console.log(error)
+    }
+  }
+
+  async function RemoveFromCart(product){
+    try{
+      const response = await axios.post("/cart/remove", { username: user.username, product: product });
+      if(response.data.success){
+        toast.error(response.data.success)
+        onAddToCart()
+      }
+    }catch(error){
+      console.log(error)
+    }
+  }
+ 
   return (
     <div className='border border-black rounded-lg p-4'>
       <img
@@ -22,7 +51,12 @@ export default function BookElement({image, name, author, price, isbn}) {
         </h2>
         <p className='text-gray-600'>{author}</p>
         <p className='text-gray-800 font-semibold'>${price-Math.floor(price) !== 0 ? price: price+".00" }</p>
-        <button className='mt-4 w-full bg-black text-white py-2 px-4 rounded-lg hover:shadow-md'>Add to Cart</button>
+        {
+          inCart ? 
+          <button className='mt-4 w-full border-2 border-black py-2 px-4 rounded-lg hover:shadow-md' onClick={() => RemoveFromCart(isbn)}>In Cart</button>:
+          <button className='mt-4 w-full bg-black text-white py-2 px-4 rounded-lg hover:shadow-md' onClick={() => AddToCart(isbn)}>Add to Cart</button>
+        }
+        
       </div>
     </div>
   );
