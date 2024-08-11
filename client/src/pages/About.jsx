ea36d9e58e5b25eb5eb7cd5c2603ca6e40032147
paradox4994 <ryanpereira499@gmail.com>
2024-08-10 21:35:12 -0400

    Init

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='px-64 pt-16 pb-4'>
+      <p className='text-xl font-bold'>Welcome to Book Haven</p>
+      <p className='text-lg pt-2'>
+        At Book Haven, we're passionate about connecting readers with the books they love. Our online bookstore is
+        dedicated to providing a vast selection of titles, competitive prices, and exceptional customer service.
+      </p>
+      <p className='text-xl font-bold pt-4'>Our Story</p>
+      <p className='text-lg pt-2'>
+        Book Haven was founded by book lovers, for book lovers. We believe that reading has the power to transform
+        lives, spark imagination, and foster a deeper understanding of the world around us. Our mission is to create a
+        welcoming and inclusive space for book enthusiasts to discover new authors, explore different genres, and build
+        a community of like-minded readers.
+      </p>
+      <p className='text-xl font-bold pt-4'>Our Values</p>
+      <ul className='pt-2'>
+        <li>
+          <p className='text-lg pt-1'>Passion for Reading: We're dedicated to sharing the joy of reading with our customers.</p>
+        </li>
+        <li>
+          <p className='text-lg pt-1'>
+            Customer Satisfaction: We strive to provide exceptional service, fast shipping, and hassle-free returns.
+          </p>
+        </li>
+        <li>
+          <p className='text-lg pt-1'>
+            Community Building: We aim to create a welcoming space for readers to connect, share, and discover new
+            books.
+          </p>
+        </li>
+        <li>
+          <p className='text-lg pt-1'>
+            Quality and Selection: We offer a curated selection of books, including bestsellers, classics, and hidden
+            gems.
+          </p>
+        </li>
+      </ul>
+      <p className='text-xl font-bold pt-4'>Meet the person behind Book Haven:</p>
+      <ul className='pt-2'>
+        <li className='text-lg pt-1'>Ryan Pereira</li>
+        <li className='text-lg pt-1'>Email: ryanpereira499@gmail.com</li>
+        <li className='text-lg pt-1'>LinkedIn:</li>
+      </ul>
+      <p className='text-xl font-bold pt-4'>Get in Touch</p>
+      <p className='text-lg pt-2'>
+        Thanks for visiting Book Haven! If you have any questions, feedback, or just want to chat about books, please
+        don't hesitate to contact us.
+      </p>
+      <p className='text-xl font-bold pt-4'>Happy Reading!</p>
+    </div>
+  );
+}
