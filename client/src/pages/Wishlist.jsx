27ab2e5ca8cad6e4d8967e82afdf924d4dc071b6
ryanpereira49 <ryanpereira499@gmail.com>
2025-02-18 22:34:46 +0000

    Added Responsiveness

--- client/src/pages/Wishlist.jsx
+++ client/src/pages/Wishlist.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect, useContext } from "react";
 import { UserContext } from "../context/userContext";
 import axios from "axios";
 import toast from "react-hot-toast";
+import BookElement from "../components/BookElement";
 
 
 export default function Wishlist() {
@@ -10,6 +11,7 @@ export default function Wishlist() {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [cart, setCart] = useState(null);
   
 
   useEffect(() => {
@@ -53,34 +55,50 @@ export default function Wishlist() {
   }
 
   return (
-    <div className='px-16 pt-4 h-svh'>
-      <p className='text-5xl font-bold'>Wishlist</p>
-      <div className='flex flex-col flex-grow'>
-        <div className='grid grid-cols-4'>
-          <p className='text-2xl font-semibold'>Book</p>
-          <p className='text-2xl font-semibold'>Author</p>
-          <p className='text-2xl font-semibold'>Category</p>
-          <p className='text-2xl font-semibold'>Action</p>
+    <div id="main-wrapper" className="flex flex-col items-center min-h-screen p-6">
+      <h1 className="text-2xl md:text-4xl text-center">Wishlist</h1>
+      <div id="desktop-wrapper" className="hidden md:flex flex-col pt-6 w-[80%]">
+        <div className="grid grid-cols-4 w-full font-bold  border-b-2 border-black text-center">
+          <p>Book</p>
+          <p>Title</p>
+          <p>Author</p>
+          <p>Action</p>
         </div>
-        {data.map((book) => (
-          <div key={book.isbn_13} className='grid grid-cols-4 pt-6'>
-            <div className='flex flex-row items-center'>
-              <img className='h-auto w-10 rounded-md' src={import.meta.env.VITE_APP_DOMAIN + book.image_sm} alt={book.title} />
-              <div className='pl-4 w-3/4'>
-                <p className='text-xl truncate'>{book.title}</p>
+        {
+          data.map((book) => (
+            <div key={book.isbn_13} className='grid grid-cols-4 pt-6 items-center p-6 text-center '>
+              <img className='h-14 rounded-md mx-auto' src={import.meta.env.VITE_APP_DOMAIN + book.image_sm} alt={book.title} />
+              <p className='text-md truncate'>{book.title}</p>
+              <p className='text-gray-600'>{book.author}</p>
+              <div>
+                <button className='py-1 px-4  text-lg text-black border-2 border-black rounded hover:shadow-md' onClick={() => handleRemove(book.isbn_13)}>Remove</button>
               </div>
             </div>
-            <div className='flex flex-row items-center'>
-                <p className='text-gray-600'>{book.author}</p>
+          ))}
+      </div>
+      <div id="mobile-wrapper" className="flex flex-col md:hidden pt-2">
+      <div className="grid grid-cols-3 font-bold border-b-2 border-black">
+          <p>Book</p>
+          <p>Title</p>
+          <p>Action</p>
+      </div>
+      <div>
+      {
+          data.map((book) => (
+            <div key={book.isbn_13} className='flex items-center justify-between pt-4 gap-x-4'>
+              <div className="flex gap-x-3 items-center">
+                <img className='h-16 rounded-md' src={import.meta.env.VITE_APP_DOMAIN + book.image_sm} alt={book.title} />
+                <div className="flex flex-col truncate">
+                  <p className='text-sm truncate'>{book.title}</p>
+                  <p className='text-gray-600 text-xs'>{book.author}</p>
               </div>
-            <div className='flex flex-row items-center'>
-              <p>{book.category}</p>
+              </div>
+              <div className="flex justify-center items-center">
+                <button className='py-1 px-2  text-lg text-black border-2 border-black rounded hover:shadow-md' onClick={() => handleRemove(book.isbn_13)}>Remove</button>
+              </div>
             </div>
-            <div className='flex flex-row items-center'>
-              <button className='py-1 px-4  text-lg text-black border-2 border-black rounded hover:shadow-md' onClick={() => handleRemove(book.isbn_13)}>Remove</button>
-            </div>
-          </div>
-        ))}
+          ))}
+      </div>
       </div>
     </div>
   );
