a3468ff514c6b8415466d9eb438c3850823e86b7
ryanpereira49 <ryanpereira499@gmail.com>
2025-02-18 05:57:27 +0000

    fixed book details overflow

--- client/src/pages/Details.jsx
+++ client/src/pages/Details.jsx
@@ -17,7 +17,8 @@ export default function Details() {
   const [error, setError] = useState(null);
   const [recom, setRecom] = useState(null);
   const [inCart, setInCart] = useState(false);
-  const [InWishlist, SetInWishlist] = useState(false)
+  const [InWishlist, SetInWishlist] = useState(false);
+  const [overflow, setOverflow] = useState("hidden");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -74,7 +75,10 @@ export default function Details() {
     const fetchRecommendations = async () => {
       if (data) {
         try {
-          const response = await axios.post("/api/book/recommendation", { category: data.category, isbn: data.isbn_13 });
+          const response = await axios.post("/api/book/recommendation", {
+            category: data.category,
+            isbn: data.isbn_13,
+          });
           setRecom(response.data);
         } catch (error) {
           setError(error);
@@ -85,51 +89,59 @@ export default function Details() {
     fetchRecommendations();
   }, [data]);
 
-  async function AddToCart(product){
-    try{
+  async function AddToCart(product) {
+    try {
       const response = await axios.post("/api/cart/add", { username: user.username, product: product });
-      if(response.data.success){
-        toast.success(response.data.success)
-        setInCart(true)
+      if (response.data.success) {
+        toast.success(response.data.success);
+        setInCart(true);
       }
-    }catch(error){
-      console.log(error)
+    } catch (error) {
+      console.log(error);
     }
   }
 
-  async function RemoveFromCart(product){
-    try{
+  async function RemoveFromCart(product) {
+    try {
       const response = await axios.post("/api/cart/remove", { username: user.username, product: product });
-      if(response.data.success){
-        toast.error(response.data.success)
-        setInCart(false)
+      if (response.data.success) {
+        toast.error(response.data.success);
+        setInCart(false);
       }
-    }catch(error){
-      console.log(error)
+    } catch (error) {
+      console.log(error);
     }
   }
 
-  async function AddToWishlist(product){
-    try{
+  async function AddToWishlist(product) {
+    try {
       const response = await axios.post("/api/wishlist/add", { username: user.username, product: product });
-      if(response.data.success){
-        toast.success(response.data.success)
-        SetInWishlist(true)
+      if (response.data.success) {
+        toast.success(response.data.success);
+        SetInWishlist(true);
       }
-    }catch(error){
-      console.log(error)
+    } catch (error) {
+      console.log(error);
     }
   }
 
-  async function RemoveFromWishlist(product){
-    try{
+  async function RemoveFromWishlist(product) {
+    try {
       const response = await axios.post("/api/wishlist/remove_min", { username: user.username, product: product });
-      if(response.data.success){
-        toast.error(response.data.success)
-        SetInWishlist(false)
+      if (response.data.success) {
+        toast.error(response.data.success);
+        SetInWishlist(false);
       }
-    }catch(error){
-      console.log(error)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  function handleOverflow() {
+    if (overflow === "hidden") {
+      setOverflow("y-scroll");
+    } else {
+      setOverflow("hidden");
     }
   }
 
@@ -142,83 +154,80 @@ export default function Details() {
   }
 
   return (
-    <div className="px-32 pt-16">
-      <div className="flex flex-row">
-        <div className="flex-shrink-0">
-          <img
-            className="w-full h-[500px] object-cover rounded-lg"
-            src={import.meta.env.VITE_APP_DOMAIN + data.image_lg}
-            alt={data.title + " cover"}
-          />
-        </div>
-        <div className="pl-8 pt-8 flex-grow">
-          <p className="text-6xl font-bold">{data.title}</p>
-          <p className="text-4xl text-gray-600 pt-3">{data.author}</p>
-          <p className="text-4xl text-gray-800 pt-3 font-semibold">
+    <div className='p-6 md:p-8'>
+      {/* Image and info wrapper */}
+      <div className='flex flex-col items-center md:items-start md:flex-row gap-3 md:gap-8'>
+        {/* Book Cover Image */}
+        <img
+          className='w-3/4 md:w-max max-h-max rounded-lg'
+          src={import.meta.env.VITE_APP_DOMAIN + data.image_sm}
+          alt={data.title + " cover"}
+        />
+        {/* Book Info Section */}
+        <div className='flex flex-col items-center md:items-start text-justify'>
+          <p className='font-bold text-2xl md:text-3xl'>{data.title}</p>
+          <p className='font-semibold pt-2 md:pt-3'>{data.author}</p>
+          <p className='font-bold text-xl pt-2 md:pt-3'>
             ${data.price - Math.floor(data.price) !== 0 ? data.price : data.price + ".00"}
           </p>
-          <div className="overflow-y-auto h-40">
-            <p className="text-xl text-gray-600 pt-3">{data.overview}</p>
-          </div>
-          <div className="flex flex-row w-full mt-8">
-            {
-              inCart ? 
-                <button className="mr-3 w-1/2 border-2 border-black py-2 px-4 rounded-lg hover:shadow-md text-xl" onClick={() => RemoveFromCart(data.isbn_13)}>
-                  Remove From Cart
-                </button>
-              :
-                <button className="mr-3 w-1/2 border-2 border-black py-2 px-4 rounded-lg hover:shadow-md text-xl" onClick={() => AddToCart(data.isbn_13)}>
-                  Add to Cart
-                </button>
-            }
-            {
-              InWishlist ?
-              <button className="ml-3 w-1/2 bg-black text-white py-2 px-4 rounded-lg hover:shadow-md text-xl" onClick={() => RemoveFromWishlist(data.isbn_13)}>
+          <div className='flex flex-col md:flex-row w-full pt-2 md:pt-4 gap-2 md:gap-4'>
+            {inCart ? (
+              <button
+                className='border-2 border-black py-2 px-4 rounded-lg hover:shadow-md'
+                onClick={() => RemoveFromCart(data.isbn_13)}>
+                Remove From Cart
+              </button>
+            ) : (
+              <button
+                className='border-2 border-black py-2 px-4 rounded-lg hover:shadow-md'
+                onClick={() => user? AddToCart(data.isbn_13): toast.error("Please login in to use cart","warning")}>
+                Add to Cart
+              </button>
+            )}
+            {InWishlist ? (
+              <button
+                className=' bg-black text-white py-2 px-4 rounded-lg hover:shadow-md'
+                onClick={() => RemoveFromWishlist(data.isbn_13)}>
                 Remove from Wishlist
               </button>
-              :
-              <button className="ml-3 w-1/2 bg-black text-white py-2 px-4 rounded-lg hover:shadow-md text-xl" onClick={() => AddToWishlist(data.isbn_13)}>
+            ) : (
+              <button
+                className=' bg-black text-white py-2 px-4 rounded-lg hover:shadow-md'
+                onClick={() => user? AddToWishlist(data.isbn_13): toast.error("Please login to use wishlist")}>
                 Add to Wishlist
               </button>
-            }
+            )}
+          </div>
+          <div className='p-2 md:p-3 mt-2 md:mt-4 border-2 border-black rounded-lg'>
+            <p className={`h-40 overflow-${overflow}`} onClick={() => handleOverflow()}>
+              {data.overview}
+            </p>
+          </div>
+          {/* Misc Info */}
+          <div className='flex flex-col md:flex-row pt-3 md:mt-4 gap-x-4'>
+            <p className=''>ISBN 13: {data.isbn_13} </p>
+            <p className=''>ISBN 10: {data.isbn_10} </p>
+            <p className=''>Category: {data.category}</p>
+            <p className=''>Publisher: {data.publisher}</p>
+            <p className=''>Page Count: {data.page_count}</p>
           </div>
         </div>
       </div>
-      <div className="mt-8">
-        <div className="flex flex-row pt-1">
-          <pre className="text-xl text-gray-800">ISBN 13: </pre>
-          <p className="text-xl text-gray-600">{data.isbn_13}</p>
-        </div>
-        <div className="flex flex-row pt-1">
-          <pre className="text-xl text-gray-800">ISBN 10: </pre>
-          <p className="text-xl text-gray-600">{data.isbn_10}</p>
-        </div>
-        <div className="flex flex-row pt-1">
-          <pre className="text-xl text-gray-800">Category: </pre>
-          <p className="text-xl text-gray-600">{data.category}</p>
-        </div>
-        <div className="flex flex-row pt-1">
-          <pre className="text-xl text-gray-800">Publisher: </pre>
-          <p className="text-xl text-gray-600">{data.publisher}</p>
-        </div>
-        <div className="flex flex-row pt-1">
-          <pre className="text-xl text-gray-800">Page Count: </pre>
-          <p className="text-xl text-gray-600">{data.page_count}</p>
-        </div>
-      </div>
-      <div className="mt-8">
-        <p className="text-4xl text-gray-800 pt-3 font-semibold">Read More</p>
-        <div className="grid grid-cols-5 gap-7 content-around mt-8 mb-8">
-          {recom && recom.map((book) => (
-            <BookElement
-              key={book.isbn_13}
-              image={book.image_sm}
-              name={book.title}
-              author={book.author}
-              price={book.price}
-              isbn={book.isbn_13}
-            />
-          ))}
+      {/* Read More */}
+      <div>
+        <p className='text-4xl text-gray-800 pt-3 font-semibold'>Read More</p>
+        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-7 content-around pt-3'>
+          {recom &&
+            recom.map((book) => (
+              <BookElement
+                key={book.isbn_13}
+                image={book.image_sm}
+                name={book.title}
+                author={book.author}
+                price={book.price}
+                isbn={book.isbn_13}
+              />
+            ))}
         </div>
       </div>
     </div>
